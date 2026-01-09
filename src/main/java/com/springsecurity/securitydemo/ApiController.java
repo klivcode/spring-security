36d6839373c0387500dc77c7978338383365826f
Bhim Raj <infobrb04@gmail.com>
2026-01-09 15:27:14 +0545

    feat(spring_security): use of in memory authentication and set the role based acces the api's

--- src/main/java/com/springsecurity/securitydemo/ApiController.java
+++ src/main/java/com/springsecurity/securitydemo/ApiController.java
@@ -1,5 +1,6 @@
 package com.springsecurity.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 @RequestMapping
-public class Greeting {
+public class ApiController {
 
 
     @GetMapping(path="/hello")
-
     public String hello() {
         return "Hello World";
+    }
+    @PreAuthorize("hasRole('ADMIN')") // used to check the authorization
+    @GetMapping(path = "/admin")
+    public String admin() {
+        return "Admin Dashboard";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping(path = "/user")
+    public String user() {
+        return "User Dashboard";
     }
 }
