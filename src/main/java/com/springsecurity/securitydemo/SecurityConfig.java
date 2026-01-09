2949bbd7170f3a591f29d3b90f3442d54914e740
Bhim Raj <infobrb04@gmail.com>
2026-01-09 17:27:02 +0545

    feat(spring_security) : schema.sql for the h2 database and use of jdbuserdetailsManager to store the users with role in memory database / locally

--- src/main/java/com/springsecurity/securitydemo/SecurityConfig.java
+++ src/main/java/com/springsecurity/securitydemo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springsecurity.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,7 +13,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,6 +25,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests((requests)->requests
@@ -42,6 +50,7 @@ public class SecurityConfig {
 
 
     // in memory Authentication
+    // hardcoded users not in practice in real world
     @Bean
     public UserDetailsService userDetailsService(){
 
@@ -58,8 +67,15 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
 
+        // jdbcUserDetailsManager is used to store the users object in database
+        JdbcUserDetailsManager userDetailsManager
+                = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
 
-        return new InMemoryUserDetailsManager(user1,admin);
+        // InMemoryUserDetailsManager is used to store the users details in memory / locally
+//        return new InMemoryUserDetailsManager(user1,admin);
     }
 
 }
